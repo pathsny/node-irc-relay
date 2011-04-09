5ce57a6006a6361bf7e6fbb45e3bfcccdfe65974
Vishnu Iyengar <pathsny@gmail.com>
2011-04-09 19:33:05 +0530

    added tell and started on seen!

--- bot.js
+++ bot.js
@@ -1,42 +1,49 @@
-var sys = require('sys');
 var irc = require('irc');
 var _ = require('./underscore');
 var commands = require('./commands')
 
-var channel = "#animestan-hell";
+var channel = "#animestan";
 var incoming = 'message' + channel;
 var nick = 'amelia';
+
+function channel_say(message) {
+    bot.say(channel, message)
+}
 
 function make_client(server) {
     var client = new irc.Client('irc.immortal-anime.net', nick, {
         channels: [channel],
     });
     client.addListener('error', function(message) {
-        sys.puts('ERROR: ' + server + ' : '+ message.command + ': ' + message.args.join(' '));
+        console.error('ERROR: ' + server + ' : '+ message.command + ': ' + message.args.join(' '));
     });
     return client;
-}
+};
 
 var bot = make_client();
 
-function dispatch(command, tokens) {
+function dispatch(command, from, tokens) {
     if (typeof(commands[command]) === 'function') {
-        commands[command](tokens, function(result) {
-            sys.puts('fuwa ' + result);
-            if (result) bot.say(result);
+        commands[command](from, tokens, function(result) {
+            console.log('fuwa ' + result);
+            if (result) channel_say(result);
         });
     }
-}
+};
 
 bot.addListener(incoming, function(from, message) {
     var tokens = message.split(' ');
     var first = _(tokens).head();
     if (first === nick) {
         var msg_tokens = _(tokens).tail();
-        dispatch(_(msg_tokens).head(), _(msg_tokens).tail())
+        dispatch(_(msg_tokens).head(), from, _(msg_tokens).tail())
     };
     var match = /(.*)!/.exec(first);
     if (match) {
-        dispatch(match[1], _(tokens).tail())
+        dispatch(match[1], from, _(tokens).tail())
     };
-})
+});
+
+_(commands.listeners(channel_say)).each(function(listener){
+    bot.addListener(incoming, listener);
+});
--- commands.js
+++ commands.js
@@ -1,5 +1,49 @@
+var _ = require('./underscore');
+
 (function(){
-    exports.g = function(tokens, cb) {
+    exports.g = function(from, tokens, cb) {
         cb("google searched for " + tokens);
-     }
+    };
+
+    var tells = {};
+
+    exports.tell = function(from, tokens, cb) {
+        var to = _(tokens).head();
+        var msg = _(tokens).tail().join(' ');
+        if (to && msg) {
+            tells[to] = _(tells[to] || []).push([from, msg])
+            cb(from + ": Message Noted");
+        } else {
+            cb("Message not understood");
+        }
+    };
+    
+    var seen_data = {};
+    
+    exports.seen = function(from, tokens, cb) {
+        var person = _(tokens).head();
+        if (person && seen_data[person]) {
+            cb(from + ": " + "Last saw " + person)
+        }
+    };
+    
+    exports.listeners = function(respond){
+        return [
+            //collect data for seen
+            function(from, message) {
+                console.log(from, message);
+                // seen_data[from] = []
+            },
+
+            //convey messages
+            function(from, message) {
+                if (tells[from]) {
+                    _(tells[from]).forEach(function(item){
+                        respond(from + ": " + item[0] + " said '" + item[1] + "'");
+                    })
+                    delete tells[from];
+                };
+            }
+        ]
+    }
 })()
